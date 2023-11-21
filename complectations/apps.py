1c3a42e4d8f9097a58de8e0f0249e99c47c63ca5
gef3dx <gef3dx@gmail.com>
2023-11-21 13:45:15 +0300

    init

--- complectations/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ComplectationsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'complectations'
-    verbose_name = 'Комплектации и товары'

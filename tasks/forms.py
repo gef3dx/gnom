4abb112359d968448e5b8f6dba50e669a058f501
gef3dx <gef3dx@gmail.com>
2023-11-23 14:39:15 +0300

    step 3

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,8 +1,20 @@
 from django import forms
 from .models import Tasks
+from users.models import CustomUser
 
 
 class TaskForm(forms.ModelForm):
+    user = forms.ChoiceField(choices=[])
+
+    def __init__(self, *args, **kwargs):
+        super(TaskForm, self).__init__(*args, **kwargs)
+        self.fields['user'].choices = self.get_staff_users()
+
+    def get_staff_users(self):
+        staff_users = CustomUser.objects.filter(is_worker=True)
+        user_choices = [(user.id, user.username) for user in staff_users]
+        return user_choices
+
     class Meta:
         model = Tasks
         fields = "__all__"
